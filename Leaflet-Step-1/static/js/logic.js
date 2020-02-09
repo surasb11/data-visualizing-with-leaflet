6721c18562905db13a83b8b0ccd6c489df929359
Sugra Baghirova <surasb11@users.noreply.github.com>
2020-02-08 20:00:02 -0500

    Visualizing Data with Leaflet

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,2 @@
+// API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
---
+++ Leaflet-Step-2/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic3VyYXNiIiwiYSI6ImNrNjhpeHltazA1bDYzbGxqcWppdXgwdjAifQ.7R36OfS0Pz407QtXa2Izsw";
